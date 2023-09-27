b1ccfad864ff05fd839e7113dcc2347d1101c275
Giero98 <b.gieras98@gmail.com>
2023-09-27 18:13:43 +0200

    Dodaj pliki projektów.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VaultOfGaming
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VaultOfGaming
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
