3e3a45fb436740feec5fca6f5e5a446f88a0c973
Giero98 <b.gieras98@gmail.com>
2023-10-31 15:53:02 +0100

    The first prototype of the list of displayed games - Added CollectionView as an item display tool - Adjust the size of graphics, text and game rating - Added sample data for testing

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,10 +1,50 @@
-﻿namespace VaultOfGaming.Views
+﻿using System.Collections.ObjectModel;
+
+namespace VaultOfGaming.Views
 {
     public partial class HomePage : ContentPage
     {
         public HomePage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
+
+        public ObservableCollection<Game> Games { get; } = new ObservableCollection<Game>
+        {
+            new Game { ImagePath = "game.png", Title = "Alan Wake II", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 2", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 1", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 2", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 1", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 2", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 1", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 2", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 1", Rate = "90"},
+            new Game { ImagePath = "game.png", Title = "Tytuł 2", Rate = "90"},
+            // Dodaj więcej elementów YourItem
+        };
+
+        private void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null && e.CurrentSelection.Count > 0)
+            {
+                if (e.CurrentSelection[0] is Game selectedItem)
+                {
+                    if(selectedItem.Title.Equals("Alan Wake II"))
+                    {
+                        Navigation.PushAsync(new MenuPage());
+                    }
+                }
+                (sender as CollectionView).SelectedItem = null;
+            }
+        }
+    }
+
+    public class Game
+    {
+        public string ImagePath { get; set; }
+        public string Title { get; set; }
+        public string Rate { get; set; }
     }
 }
