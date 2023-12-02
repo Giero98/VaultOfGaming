e320ae160fa7ec85b043016f584c7810f34232d7
Bartosz Gieras <b.gieras98@gmail.com>
2023-12-02 20:15:37 +0100

    - Update background image on GameCover on HomePage - Add function to pressed searchbutton

--- Views/SearchPage.xaml.cs
+++ Views/SearchPage.xaml.cs
@@ -1,11 +1,19 @@
-﻿namespace VaultOfGaming.Views
+﻿using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+
+namespace VaultOfGaming.Views
 {
     public partial class SearchPage : ContentPage
     {
-
         public SearchPage()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void OnSearchButtonPressed(object sender, EventArgs e)
+        {
+
         }
     }
 }
